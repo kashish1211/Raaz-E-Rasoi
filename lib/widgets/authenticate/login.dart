251d5126c3b850729ca20daf4c11873dbdf96756
kashish1211 <kashish.h@somaiya.edu>
2021-10-12 23:51:12 +0530

    added authenticaion
    Co-authored-by: shubh1605 <shubh1605@users.noreply.github.com>
    Co-authored-by: HarshitJain28 <HarshitJain28@users.noreply.github.com>

--- lib/widgets/authenticate/login.dart
+++ lib/widgets/authenticate/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:raaz_e_rasoi/widgets/home.dart';
 
 import 'header.dart';
@@ -21,134 +22,161 @@ class _LoginState extends State<Login> {
   String _userEmail = '';
   String _userPassword = '';
 
-  void _trySubmit() {
+  var _isLoading = false;
+
+  void _trySubmit(BuildContext ctx) {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
     if (isValid) {
       _formKey.currentState!.save();
-      login();
+      login(ctx);
     }
   }
 
-  void login() async {
-    UserCredential authResult;
-    authResult = await _auth.signInWithEmailAndPassword(
-        email: _userEmail, password: _userPassword);
+  void login(BuildContext ctx) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      UserCredential authResult;
+      authResult = await _auth.signInWithEmailAndPassword(
+        email: _userEmail,
+        password: _userPassword,
+      );
+      Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+        return MyHome();
+      }));
+    } on FirebaseAuthException catch (e) {
+      String? message = e.message;
+
+      setState(() {
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message!),
+          backgroundColor: Colors.red,
+        ),
+      );
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData;
     queryData = MediaQuery.of(context);
-    return Material(
-      child: Container(
-        margin: EdgeInsets.only(top: queryData.size.height * 0.4),
-        height: queryData.size.height * 0.6,
-        decoration: BoxDecoration(
-          color: Color(0xfff2f2f2),
-        ),
-        child: ListView(
-          children: [
-            // Header(),
+    return Scaffold(
+      body: Material(
+        child: Container(
+          margin: EdgeInsets.only(top: queryData.size.height * 0.4),
+          height: queryData.size.height * 0.6,
+          decoration: BoxDecoration(
+            color: Color(0xfff2f2f2),
+          ),
+          child: ListView(
+            children: [
+              // Header(),
 
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 35),
-              padding: EdgeInsets.only(
-                bottom: queryData.viewInsets.bottom + 10,
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    SizedBox(height: queryData.size.height * 0.04),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || !value.contains('@')) {
-                          return 'Enter valid email address';
-                        }
-                        return null;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email address',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 18,
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: const Color(0xfffa4a0c)),
-                        ),
-                      ),
-                      onSaved: (value) {
-                        _userEmail = value!;
-                      },
-                      // onSubmitted: (_) => {},
-                    ),
-                    SizedBox(height: queryData.size.height * 0.04),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || value.length < 7) {
-                          return 'Too Short!';
-                        }
-                        return null;
-                      },
-                      obscureText: true,
-                      obscuringCharacter: "*",
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 18,
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: const Color(0xfffa4a0c)),
-                        ),
-                      ),
-                      onSaved: (value) {
-                        _userPassword = value!;
-                      },
-                      // onSubmitted: (_) => {},
-                    ),
-                    SizedBox(height: queryData.size.height * 0.01),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          child: Text(
-                            'Forgot Password?',
-                            style: TextStyle(
-                              color: Color(0xffff460a),
-                            ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 35),
+                padding: EdgeInsets.only(
+                  bottom: queryData.viewInsets.bottom + 10,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(height: queryData.size.height * 0.04),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty || !value.contains('@')) {
+                            return 'Enter valid email address';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email address',
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 18,
                           ),
-                          onPressed: () => {},
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: const Color(0xfffa4a0c)),
+                          ),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: queryData.size.height * 0.08),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0xffff460a),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
+                        onSaved: (value) {
+                          _userEmail = value!;
+                        },
+                        // onSubmitted: (_) => {},
+                      ),
+                      SizedBox(height: queryData.size.height * 0.04),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 7) {
+                            return 'Too Short!';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 18,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: const Color(0xfffa4a0c)),
+                          ),
+                        ),
+                        onSaved: (value) {
+                          _userPassword = value!;
+                        },
+                        // onSubmitted: (_) => {},
+                      ),
+                      SizedBox(height: queryData.size.height * 0.01),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          TextButton(
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: Color(0xffff460a),
+                              ),
+                            ),
+                            onPressed: () => {},
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: queryData.size.height * 0.08),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xffff460a),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                        onPressed: () => _trySubmit(context),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
                         ),
                       ),
-                      onPressed: _trySubmit,
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/authenticate/signup.dart
+++ lib/widgets/authenticate/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:raaz_e_rasoi/widgets/authenticate/header.dart';
 
 import '../home.dart';
 
@@ -16,29 +18,60 @@ class _SignupState extends State<Signup> {
   String _userFullname = '';
   String _userPassword = '';
 
+  var _isLoading = false;
+
   void getMyHome(BuildContext ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
       return MyHome();
     }));
   }
 
-  void _trySubmit() {
+  void _trySubmit(BuildContext ctx) {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
     if (isValid) {
       _formKey.currentState!.save();
-      signup();
+      signup(ctx);
     }
   }
 
-  void signup() async {
+  void signup(BuildContext ctx) async {
     try {
+      setState(() {
+        _isLoading = true;
+      });
       UserCredential authResult;
       authResult = await _auth.createUserWithEmailAndPassword(
           email: _userEmail, password: _userPassword);
-    } on PlatformException catch (e) {
-      // var message = "An error has occured."
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(authResult.user!.uid)
+          .set(
+        {
+          'FullName': _userFullname,
+          'email': _userEmail,
+        },
+      );
 
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+        return Header();
+      }));
+    } on FirebaseException catch (e) {
+      String? message = e.message;
+
+      setState(() {
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message!),
+          backgroundColor: Colors.red,
+        ),
+      );
+      print(e);
     }
   }
 
@@ -47,124 +80,128 @@ class _SignupState extends State<Signup> {
     MediaQueryData queryData;
     queryData = MediaQuery.of(context);
 
-    return Material(
-      child: Container(
-        margin: EdgeInsets.only(top: queryData.size.height * 0.4),
-        height: queryData.size.height * 0.6,
-        decoration: BoxDecoration(
-          color: Color(0xfff2f2f2),
-        ),
-        child: ListView(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 35),
-              padding: EdgeInsets.only(
-                bottom: queryData.viewInsets.bottom + 10,
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    SizedBox(height: queryData.size.height * 0.03),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter your good name';
-                        }
-                        return null;
-                      },
+    return Scaffold(
+      body: Material(
+        child: Container(
+          margin: EdgeInsets.only(top: queryData.size.height * 0.4),
+          height: queryData.size.height * 0.6,
+          decoration: BoxDecoration(
+            color: Color(0xfff2f2f2),
+          ),
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 35),
+                padding: EdgeInsets.only(
+                  bottom: queryData.viewInsets.bottom + 10,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(height: queryData.size.height * 0.03),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter your good name';
+                          }
+                          return null;
+                        },
 
-                      decoration: InputDecoration(
-                        labelText: 'Full Name',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 15,
+                        decoration: InputDecoration(
+                          labelText: 'Full Name',
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 15,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: const Color(0xfffa4a0c)),
+                          ),
                         ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: const Color(0xfffa4a0c)),
-                        ),
-                      ),
-                      onSaved: (value) {
-                        _userFullname = value!;
-                      },
+                        onSaved: (value) {
+                          _userFullname = value!;
+                        },
 
-                      // onSubmitted: (_) => {},
-                    ),
-                    SizedBox(height: queryData.size.height * 0.03),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || !value.contains('@')) {
-                          return 'Enter valid email address';
-                        }
-                        return null;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email address',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 15,
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: const Color(0xfffa4a0c)),
-                        ),
+                        // onSubmitted: (_) => {},
                       ),
-                      onSaved: (value) {
-                        _userEmail = value!;
-                      },
-                      // onSubmitted: (_) => {},
-                    ),
-                    SizedBox(height: queryData.size.height * 0.03),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || value.length < 7) {
-                          return 'Too Short!';
-                        }
-                        return null;
-                      },
-                      obscureText: true,
-                      obscuringCharacter: "*",
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 15,
+                      SizedBox(height: queryData.size.height * 0.03),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty || !value.contains('@')) {
+                            return 'Enter valid email address';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email address',
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 15,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: const Color(0xfffa4a0c)),
+                          ),
                         ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: const Color(0xfffa4a0c)),
-                        ),
+                        onSaved: (value) {
+                          _userEmail = value!;
+                        },
+                        // onSubmitted: (_) => {},
                       ),
-                      onSaved: (value) {
-                        _userPassword = value!;
-                      },
-                      // onSubmitted: (_) => {},
-                    ),
-                    SizedBox(height: queryData.size.height * 0.08),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0xffff460a),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
+                      SizedBox(height: queryData.size.height * 0.03),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 7) {
+                            return 'Too Short!';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 15,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: const Color(0xfffa4a0c)),
+                          ),
                         ),
+                        onSaved: (value) {
+                          _userPassword = value!;
+                        },
+                        // onSubmitted: (_) => {},
                       ),
-                      onPressed: _trySubmit,
-                      child: Text(
-                        "Sign up",
-                        style: TextStyle(
-                          fontSize: 22,
+                      SizedBox(height: queryData.size.height * 0.08),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xffff460a),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
                         ),
+                        onPressed: () => _trySubmit(context),
+                        child: _isLoading
+                            ? CircularProgressIndicator()
+                            : Text(
+                                "Sign up",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
